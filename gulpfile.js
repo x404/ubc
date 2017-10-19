ffd973c7a2b0305b9942e311876ef42b7ac032f9
x404 <kronos2003@gmail.com>
2017-10-19 13:17:59 +0300

    Разметка главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('css-libs', ['scss'],  function(){
 // автоперезагрузка страницы браузера
 gulp.task('browser-sync', function(){
 	browserSync({
-		proxy: "subdomain.cryocream.local",
+		proxy: "ubc.local",
 		notify: false
 	})
 });
